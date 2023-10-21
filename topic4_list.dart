c87a21e28c42938150425f508d680c46b41cd70e
Akniyet <131252267+Akaltoshka05@users.noreply.github.com>
2023-10-21 14:39:48 +0600

    Update topic4_list.dart

--- topic4_list.dart
+++ topic4_list.dart
@@ -1,3 +1,47 @@
+//[Task 1]
+//Дан массив. Все его элементы:
+//а) увеличить в 2 раза;
+//б) уменьшить на число А;
+//в) разделить на первый элемент.
+// void main (){ 
+//   var list = [3, 6, 9, 12 ]; 
+//   list.forEach((number)=>print(number*2)); 
+// } 
+// //Task 1 (b) 
+// void main (){ 
+//   int A = 2; 
+//   var list = [6, 10, 14, 18, 22]; 
+//   list.forEach((number)=>print(number-A)); 
+// } 
+//Task 1 (c) 
+// void main (){ 
+//   int A = 2; 
+//   var list = [6, 10, 14, 18, 221]; 
+//   list.forEach((number)=>print(number/list[0])); 
+// } 
+//[Task 2]
+//Дан массив. Все его элементы:
+//а) уменьшить на 20;
+//б) умножить на последний элемент;
+//в) увеличить на число В.
+// void main (){ 
+//   int A = 2; 
+//   var list = [60, 2750, 22, 12]; 
+//   list.forEach((number)=>print(number-20)); 
+// } 
+ 
+//Task 2(b) 
+// void main (){ 
+//   int A = 2; 
+//   var list = [60, 2750, 22, 12]; 
+//   list.forEach((number)=>print(number*(list[3]))); 
+// } 
+ 
+// //Task 2(c) 
+// void main (){ 
+//   int B = 5; 
+//   var list = [60, 2750, 22, 12]; 
+//   list.forEach((number)=>print(number+B)); 
 //Определить:
 //а) сумму всех элементов массива;
 //б) произведение всех элементов массива;
